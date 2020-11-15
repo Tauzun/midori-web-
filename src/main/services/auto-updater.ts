4d33e3970e7ff12aa92a79b7de8fbc99d62bd03d
ponchale1 <alfonso.hernandez@astian.org>
2020-11-15 14:53:42 -0500

    Revert "update user agent for update"
    This reverts commit 46ab5c95abfcc769cac3493a4f40efb5d0a4a46b.

--- src/main/services/auto-updater.ts
+++ src/main/services/auto-updater.ts
@@ -3,27 +3,17 @@ import { ipcMain } from 'electron';
 import { WindowsManager } from '../windows-manager';
 
 export const runAutoUpdaterService = (windowsManager: WindowsManager) => {
-  let updateAvailable = false;
-
-  ipcMain.on('install-update', () => {
-    if (process.env.NODE_ENV !== 'development') {
-      autoUpdater.quitAndInstall(true, true);
-    }
-  });
-
-  ipcMain.handle('is-update-available', () => {
-    return updateAvailable;
+  ipcMain.on('update-install', () => {
+    autoUpdater.quitAndInstall();
   });
 
   ipcMain.on('update-check', () => {
     autoUpdater.checkForUpdates();
   });
 
-  autoUpdater.on('update-downloaded', () => {
-    updateAvailable = true;
+  autoUpdater.on('update-downloaded', ({ version }) => {
     for (const window of windowsManager.list) {
-      window.webContents.send('update-available');
-      window.dialogs.menuDialog.webContents.send('update-available');
+      window.webContents.send('update-available', version);
     }
   });
 };
